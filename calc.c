040f46da2eeba2472f97781df537b10ba86f4e73
mrpre <mrpre@163.com>
2018-08-30 20:56:07 +0800

    support ec pub calc

--- calc.c
+++ calc.c
@@ -1,10 +1,86 @@
-#include <openssl/bn.h>
+#include <stdio.h>
 #include <string.h>
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/objects.h>
+
 #define MAX_BYTES 1024
 unsigned char a[MAX_BYTES];
 unsigned char b[MAX_BYTES];
 unsigned char c[MAX_BYTES];
 unsigned char d[MAX_BYTES];
+
+void ec_mul_pub(unsigned char *curve, unsigned char *private)
+{
+        EC_POINT *r = NULL;
+        EC_GROUP *group = NULL;
+        BN_CTX *ctx = NULL;
+        BIGNUM *pkey;
+        int i, nid, len;
+
+        if(NULL == (pkey = BN_new())) {
+            printf("BN_new err\n");
+            return;
+        }
+
+        if (NULL == (ctx = BN_CTX_new())) {
+            printf("BN_CTX_new err\n");
+            return;
+        }
+
+        if(!BN_hex2bn(&pkey, private))
+        {
+            printf("BN_hex2bn err\n");
+            return;
+        }
+
+        if (!strncmp("secp192r1", curve, sizeof("secp192r1") - 1))
+            nid = NID_X9_62_prime192v1;
+        else if (!strncmp("secp256r1",curve, sizeof("secp256r1") - 1))
+            nid = NID_X9_62_prime256v1;
+        else 
+            nid = OBJ_sn2nid(curve);
+
+        if (nid == 0)
+            nid = EC_curve_nist2nid(curve);
+        if (nid == 0) {
+            printf("unknow curve\n");
+            return; 
+        }
+
+        group = EC_GROUP_new_by_curve_name(nid);
+
+        if (NULL == (r = EC_POINT_new(group))) {
+            printf("EC_POINT_new err\n");
+            return;
+        }
+
+        if (!EC_POINT_mul(group, r, pkey, NULL, NULL, ctx)) {
+            printf("EC_POINT_mul err\n");
+            return;
+        }
+
+        len = EC_POINT_point2oct(group, r, POINT_CONVERSION_UNCOMPRESSED, NULL, 0, NULL);
+        if (!len) {
+            printf("EC_POINT_point2oct error 1\n");
+            return;
+        }
+        printf("point len:%d\n", len);
+        if (!EC_POINT_point2oct(group, r, POINT_CONVERSION_UNCOMPRESSED, a, len, NULL)) {
+            printf("EC_POINT_point2oct error 2\n");
+            return;
+        }
+
+        printf("x:");
+        for (i = 1 ; i < (len-1)/2 + 1; i++) {
+            printf("%02x", a[i]);
+        }
+        printf("\ny:");
+        for (i = (len-1)/2 + 1 ; i < len; i++) {
+            printf("%02x", a[i]);
+        }
+        printf("\n");
+}
 
 void mod_inv(unsigned char *a, unsigned char *p)
 {
@@ -133,7 +209,7 @@ void mul_and_div(unsigned char *a, unsigned char *p, unsigned char *m)
         BN_clear(ba);
         BN_clear(bb);
         BN_div(ba, bb, bd, bc, ctx);
-        printf("quotient:0x%s remainder:0x%s",BN_bn2hex(ba), BN_bn2hex(bb));
+        printf("Result divided by %s equal to :quotient:0x%s remainder:0x%s",m,BN_bn2hex(ba), BN_bn2hex(bb));
         printf("\n");
 
 }
@@ -161,12 +237,15 @@ void main(int argc, unsigned char **argv)
             mul_and_div(argv[2], argv[3], "01");
         else if (!strncmp("div", argv[1], 3) && argc == 4)
             mul_and_div(argv[2], "01", argv[3]);
+        else if (!strncmp("ec_mul", argv[1], 3) && argc == 4)
+            ec_mul_pub(argv[2], argv[3]);
         else
             goto help;
         return ;
 help:
        printf("Usage:\n");
-       printf("To do [0aff ^ 0bab mod 0c1f]:          ./a.out mod_exp \"0aff' \"0bab\" \"0c1f\" \n");
-       printf("To do [0a ^ -1 mod 0b]:                ./a.out mod_inv \"0a' \"0b\" \n");
-       printf("To do [0a mod/add/sub/mul/div/exp 0b]: ./a.out mod/add/sub/mul/div/exp \"0a' \"0b\" \n");
+       printf("To do [0aff ^ 0bab mod 0c1f]:          ./calc.o mod_exp \"0aff' \"0bab\" \"0c1f\" \n");
+       printf("To do [0a ^ -1 mod 0b]:                ./calc.o mod_inv \"0a' \"0b\" \n");
+       printf("To do [0a mod/add/sub/mul/div/exp 0b]: ./calc.o mod/add/sub/mul/div/exp \"0a' \"0b\" \n");
+       printf("To do ec pub calc:                     ./calc.o ec_mulsecp 256k1 \"abcdefg..\"\n");
 }
